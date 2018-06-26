f54555921b5c656e9d0dd79ba18218974d7acab4
jonmoon74 <jon.moon@outlook.com>
2018-06-26 20:14:14 +0100

    Setting up the main UI and second view controller

---
+++ Sound Board/SoundViewController.swift
@@ -0,0 +1,33 @@
+//
+//  SoundViewController.swift
+//  Sound Board
+//
+//  Created by Jon Moon on 26/06/2018.
+//  Copyright © 2018 Jon Moon. All rights reserved.
+//
+
+import UIKit
+
+class SoundViewController: UIViewController {
+
+    @IBOutlet weak var recordButton: UIButton!
+    
+    @IBOutlet weak var nameTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func recordTapped(_ sender: Any) {
+    }
+    
+    @IBAction func playTapped(_ sender: Any) {
+    }
+    
+    @IBAction func addTapped(_ sender: Any) {
+    }
+    
+
+}

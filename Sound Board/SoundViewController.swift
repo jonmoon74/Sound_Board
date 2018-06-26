bfee7710d04e16f6bb0aff4871193e49f400f949
jonmoon74 <jon.moon@outlook.com>
2018-06-26 20:47:23 +0100

    Starting to work on the aduio session but need to add try statements to the throws still

--- Sound Board/SoundViewController.swift
+++ Sound Board/SoundViewController.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import AVFoundation
+
 
 class SoundViewController: UIViewController {
 
@@ -14,10 +16,28 @@ class SoundViewController: UIViewController {
     
     @IBOutlet weak var nameTextField: UITextField!
     
+    var audioRecorder : AVAudioRecorder?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        setupRecorder()
+    }
+    
+    func setupRecorder() {
+        //create an audio session
+        let session = AVAudioSession.sharedInstance()
+        session.setCategory(AVAudioSessionCategoryPlayAndRecord)
+        session.overrideOutputAudioPort(.speaker)
+        session.setActive(true)
+        
+        
+        //create url for audio file
+        
+        // create settings for audioRecorder
+        
+        //create audioRecorder object
+        audioRecorder = AVAudioRecorder(url: <#T##URL#>, settings: <#T##[String : Any]#>)
     }
 
     @IBAction func recordTapped(_ sender: Any) {
